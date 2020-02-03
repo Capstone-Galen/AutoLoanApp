2f71892f98d8cc883b8212bc02461871ac680b68
Galen Schatzman <gschatzman@student.neumont.edu>
2020-02-03 11:21:13 -0700

    Changes to linux version and main.py to run supported linux format

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from decimal import *
+import decimal as dc
 # Auto Loan Calculator console representation
 # Created by Galen Schatzman 12/26/2019
 #Now added to repository at https://github.com/SchatzmanGPS/ConsoleLoanApp.git
 g = input("Enter the amount in dollars you would like to borrow from the bank : ")
-g = Decimal(g)
+g = dc.Decimal(g)
 h = input("Enter the loan interest rate (i.e. 4.49): ")
-h = Decimal(h)
+h = dc.Decimal(h)
 i = input("Enter the term of your loan in years : ")
-i = Decimal(i)
+i = dc.Decimal(i)
 print("Calculating...")
 print("")
 
 interestRate = (h / 100)
-interestRate = Decimal(interestRate)
+interestRate = dc.Decimal(interestRate)
 monthCalc = (i * 12)
-monthCalc = Decimal(monthCalc)
+monthCalc = dc.Decimal(monthCalc)
 interestXComp = (interestRate / 1)
-interestXComp = Decimal(interestXComp)
+interestXComp = dc.Decimal(interestXComp)
 a = (1 + interestXComp)
 b = (1 * i)
 #POW = pow(a,b) Line 23 is also a exponential expression
@@ -40,4 +40,5 @@ print("")
 print("Your minimum monthly payment will be: $" + m)
 print("")
 print("You will be paying the above amount for " + monthCalc + " months.")
+print("")
 
